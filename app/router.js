5b51d8414f77094e0b9879fe0f24c3cb93b7eaad
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-02-19 17:31:39 +0100

    Add quiz details view

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('quiz', { path: "/quiz"});
+  this.resource('quiz', {path: "/quiz"}, function() {
+    this.route('show', {path: "/:quiz_id"});
+  });
 });
 export default Router;
