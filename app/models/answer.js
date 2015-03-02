c60c6cd372717f50b6cd3bf2f3ee966d0caecb2d
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-03-02 13:39:35 +0100

    Add correct attr to answers and visible attr to question. Show just visible question

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,23 +2,24 @@ import DS from 'ember-data';
 
 var Answer = DS.Model.extend({
   title: DS.attr('string'),
-  question: DS.belongsTo('question')
+  question: DS.belongsTo('question'),
+  correct: DS.attr('boolean'),
 });
 
 Answer.reopenClass({
   FIXTURES: [
-    { id: 1, question_id: 1, title: '42' },
-    { id: 2, question_id: 1, title: 'Peace, Love and Joy' },
-    { id: 3, question_id: 1, title: '43' },
-    { id: 4, question_id: 1, title: '44' },
-    { id: 5, question_id: 2, title: 'God' },
-    { id: 6, question_id: 2, title: 'Your Brain' },
-    { id: 7, question_id: 2, title: 'The Pimpmaster' },
-    { id: 8, question_id: 2, title: 'Santa' },
-    { id: 9, question_id: 3, title: 'Fulfill the prophecy' },
-    { id: 10, question_id: 3, title: 'Plant a tree' },
-    { id: 11, question_id: 3, title: 'Doing that damm quiz' },
-    { id: 12, question_id: 3, title: "I don't care" },
+    { id: 1, question_id: 1, title: '42', correct: true },
+    { id: 2, question_id: 1, title: 'Peace, Love and Joy', correct: false },
+    { id: 3, question_id: 1, title: '43', correct: false },
+    { id: 4, question_id: 1, title: '44', correct: false },
+    { id: 5, question_id: 2, title: 'God', correct: false },
+    { id: 6, question_id: 2, title: 'Your Brain', correct: true },
+    { id: 7, question_id: 2, title: 'The Pimpmaster', correct: false },
+    { id: 8, question_id: 2, title: 'Santa', correct: false },
+    { id: 9, question_id: 3, title: 'Fulfill the prophecy', correct: false },
+    { id: 10, question_id: 3, title: 'Plant a tree', correct: false },
+    { id: 11, question_id: 3, title: 'Doing that damm quiz', correct: true },
+    { id: 12, question_id: 3, title: "I don't care", correct: false },
   ]
 });
 
--- app/models/question.js
+++ app/models/question.js
@@ -3,17 +3,18 @@ import DS from 'ember-data';
 var Question = DS.Model.extend({
   title: DS.attr('string'),
   quiz: DS.belongsTo('quiz'),
-  answers: DS.hasMany('answer', {async: true})
+  answers: DS.hasMany('answer', {async: true}),
+  visible: DS.attr('boolean'), 
 });
 
 Question.reopenClass({
   FIXTURES: [
-    { id: 1, quiz_id: 1, title: 'What is the answer of everything?', answers: [1, 2, 3, 4] },
-    { id: 2, quiz_id: 1, title: 'Who are you?', answers: [5, 6, 7, 8] },
-    { id: 3, quiz_id: 1, title: 'Why are we here?', answers: [9, 10, 11, 12] },
-    { id: 4, quiz_id: 2, title: 'What are security issues in the internet?' },
-    { id: 5, quiz_id: 2, title: 'Why was security not a big topic when the internet was created?' },
-    { id: 6, quiz_id: 2, title: 'What are possible security attacks?' },
+    { id: 1, quiz_id: 1, title: 'What is the answer of everything?', answers: [1, 2, 3, 4], visible: true },
+    { id: 2, quiz_id: 1, title: 'Who are you?', answers: [5, 6, 7, 8], visible: false },
+    { id: 3, quiz_id: 1, title: 'Why are we here?', answers: [9, 10, 11, 12], visible: false },
+    { id: 4, quiz_id: 2, title: 'What are security issues in the internet?', visible: true },
+    { id: 5, quiz_id: 2, title: 'Why was security not a big topic when the internet was created?', visible: false },
+    { id: 6, quiz_id: 2, title: 'What are possible security attacks?', visible: false },
   ]
 });
 
