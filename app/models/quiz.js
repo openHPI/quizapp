e90e043e78236f7df929d399acca73d680d303bb
Jan Renz <jan.renz@hpi.de>
2015-03-03 10:47:08 +0100

    Begin styling

--- app/models/quiz.js
+++ app/models/quiz.js
@@ -3,12 +3,13 @@ import DS from 'ember-data';
 var Quiz = DS.Model.extend({
   title: DS.attr('string'),
   questions: DS.hasMany('question'),
+  users: DS.hasMany('user'),
   languague: DS.attr('string')
 });
 
 Quiz.reopenClass({
   FIXTURES: [
-    { id: 1, title: 'Quiz A', questions: [1, 2, 3] },
+    { id: 1, title: 'Quiz A', questions: [1, 2, 3], user: [1] },
     { id: 2, title: 'Quiz B', questions: [4, 5, 6] },
   ]
 });
