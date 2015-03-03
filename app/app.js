14e5224b87f4d8c21b8508d16629365a46b08eeb
Jan Renz <jan.renz@hpi.de>
2015-03-03 17:20:17 +0100

    display qr codes

--- app/app.js
+++ app/app.js
@@ -8,7 +8,15 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 var App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver: Resolver,
+  LOG_TRANSITIONS: true,
+  LOG_VIEW_LOOKUPS: true,
+  LOG_RESOLVER: true,
+// Extremely detailed logging, highlighting every internal
+// step made while transitioning into a route, including
+// `beforeModel`, `model`, and `afterModel` hooks, and
+// information about redirects and aborted transitions
+  LOG_TRANSITIONS_INTERNAL: true
 });
 loadInitializers(App, config.modulePrefix);
 
