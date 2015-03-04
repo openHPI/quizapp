c5103e9b5340945b55e0438dd3156a28a67725b8
Jan Renz <jan.renz@hpi.de>
2015-03-04 10:58:39 +0100

    Demo progress bar

--- app/components/progress-bar.js
+++ app/components/progress-bar.js
@@ -1,6 +1,9 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  actions: {
-  }
+  renderElement: function() {
+
+    this.$('.bar').animate({width: "0px"}, 10000, 'linear')
+
+   }.on('didInsertElement')
 });
