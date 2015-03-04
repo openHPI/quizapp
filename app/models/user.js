c960bfd1c1aabfdccd1468ddec6b9fd619eada2a
Jan Renz <jan.renz@hpi.de>
2015-03-04 13:55:04 +0100

    Top List

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,14 @@
 import DS from 'ember-data';
 
 var User = DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  points: DS.attr('integer', { defaultValue: 0 }),
 });
 
 User.reopenClass({
   FIXTURES: [
-    { id: 1, name: 'Joe' },
-    { id: 2, name: 'Jane' },
+    { id: 1, name: 'Joe', points: 3 },
+    { id: 2, name: 'Jane', points: 2 },
   ]
 });
 
