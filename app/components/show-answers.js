b8b4e7451b2b9b27c2b00d65cb98090a2524db6a
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-03-11 11:20:05 +0100

    To #11: Show answers with a delay, css changes

--- app/components/show-answers.js
+++ app/components/show-answers.js
@@ -1,6 +1,12 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+  addCssClass: function() {
+  	setTimeout( function() {
+    	this.$('.answer').animate({opacity: "1"}, 500, 'linear');
+  	}, 3000);
+   }.on('didInsertElement'),
+
   actions: {
     selectAnswer: function(answer_id, question_id) {
     	this.sendAction('action', answer_id, question_id);
