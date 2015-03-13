26a525700162d3c0431dbd4e7e40abbd3795f93f
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-03-13 21:27:26 +0100

    new ws socket url

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,7 @@ import socketMixin from 'ember-websockets/mixins/sockets';
 export default Ember.Route.extend(socketMixin, {
   socketConfigurations: [{
     key: 'socket1',
-    socketURL: 'ws://openhpi-quizapp-server.herokuapp.com:8080',
+    socketURL: 'ws://janrenz.de:82/',
     keepSocketAlive: true
 	}],
 });
