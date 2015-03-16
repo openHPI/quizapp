b2e4c7747b96f8250009261f312ad22f8c099d41
Tino Junge <tino.junge@student.hpi.uni-potsdam.de>
2015-03-16 01:00:52 +0100

    Fix QR code layout

--- app/components/qr-code.js
+++ app/components/qr-code.js
@@ -3,9 +3,9 @@ import Ember from "ember";
 export default Ember.Component.extend({
   renderQrCode: function() {
     var link = this.$('.qrcode').closest('a').attr('href');
-    if (link === '#') { link = window.location.pathname }
+    if (link === '#') { link = window.location.pathname; }
     var img = window.qr.image(window.location.origin + link);
-    this.$('.qrcode').replaceWith(img);
+    this.$('.qrcode').append(img);
 
    }.on('didInsertElement')
 });
