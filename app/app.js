bf6fa9c1b43bd0c678572e196f4c60e0de3684e7
Franz Liedke <franz.liedke@student.hpi.de>
2017-03-07 15:00:39 +0100

    Try to update to Ember 2.11

--- app/app.js
+++ app/app.js
@@ -1,23 +1,18 @@
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from './resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+
+let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-var App = Ember.Application.extend({
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver,
-  LOG_TRANSITIONS: false,
-  LOG_VIEW_LOOKUPS: false,
-  LOG_RESOLVER: false,
-// Extremely detailed logging, highlighting every internal
-// step made while transitioning into a route, including
-// `beforeModel`, `model`, and `afterModel` hooks, and
-// information about redirects and aborted transitions
-  LOG_TRANSITIONS_INTERNAL: false
+  Resolver
 });
+
 loadInitializers(App, config.modulePrefix);
 
 export default App;
--- app/router.js
+++ app/router.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-var Router = Ember.Router.extend({
-  location: config.locationType
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
@@ -16,4 +17,5 @@ Router.map(function() {
     });
   });
 });
+
 export default Router;
