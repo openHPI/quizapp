248c9c1e3d49f032247bbca28dd42c1a3cb68929
Franz Liedke <franz.liedke@student.hpi.de>
2017-03-09 17:36:38 +0100

    Handle keyboard input for matching user answers

--- app/components/show-answers.js
+++ app/components/show-answers.js
@@ -1,13 +1,28 @@
-import Ember from "ember";
+import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    setTimeout(() => this.$('.answer').animate({opacity: '1'}, 500, 'linear'), 3000);
+    setTimeout(
+      () => this.$('.answer').animate({opacity: '1'}, 500, 'linear'),
+      3000
+    );
   },
 
+  answers: Ember.computed('question.answers', function() {
+    return this.get('question.answers').map((answer, index) => ({
+      text: answer.get('title'),
+      color: ['yellow', 'green', 'red', 'blue'][index],
+      userKeys: {
+        'user1': ['ArrowUp', 'ArrowLeft', 'ArrowDown', 'ArrowRight'][index],
+        'user2': ['KeyW', 'KeyA', 'KeyS', 'KeyD'][index]
+      },
+      model: answer
+    }));
+  }),
+
   actions: {
-    selectAnswer(answer_id, question_id) {
-      this.sendAction('action', answer_id, question_id);
+    selectAnswer() {
+      this.sendAction('action', ...arguments);
     }
   }
 });
--- app/controllers/question/index.js
+++ app/controllers/question/index.js
@@ -1,25 +1,27 @@
-import Ember from "ember";
+import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['quiz/index','application'],
   currentUser: Ember.computed.alias('controllers.application.currentUser'),
   quiz: Ember.computed.alias('controllers.quiz/index.model'),
 
+  registerAnswer(answer) {
+    const question = answer.get('question');
+
+    question.set('submitted', true);
+    answer.set('selected', true);
+
+    this.transitionToRoute('question.waiting', question);
+  },
+
   actions: {
-    answerSelector(answer, question_id) {
-      answer.set('selected', true);
-      let correctAnswer = false;
-      this.store.find("question", question_id).then(question => {
-        question.set('submitted', true);
-        if (answer.get("correct") === true) {
-          correctAnswer = true;
-        }
-        this.send('emit', {question_answered: this.model.id, quiz_id: this.model.get('quiz').id, correct_answer: correctAnswer, answer_id: answer.id}, true, 'socket1');
-        this.transitionToRoute('question.waiting', question);
-      });
+    keyPressed() {
+      console.log('keyPressed', ...arguments);
+    },
+    selectAnswer(answer) {
+      this.registerAnswer(answer);
     },
     timeUp() {
-      //this.send('emit', {question_answered: this.model.id, quiz_id: this.model.get('quiz').id, correct_answer: false, answer_id: null}, true, 'socket1');
       this.transitionToRoute('question.waiting', this.model);
     },
     resetCountdown() {
