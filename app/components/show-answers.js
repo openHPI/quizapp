7f9d3e6a3231e90c8d1247c5ab4419ad30c94e74
Franz Liedke <franz.liedke@student.hpi.de>
2017-03-10 11:22:48 +0100

    Make sure timeout is always cleared

--- app/components/show-answers.js
+++ app/components/show-answers.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  timeout: null,
+
   didInsertElement() {
-    setTimeout(
+    this.timeout = setTimeout(
       () => this.$('.answer').animate({opacity: '1'}, 500, 'linear'),
       3000
     );
+  },
+
+  willDestroyElement() {
+    clearTimeout(this.timeout);
   },
 
   answers: Ember.computed('question.answers', function() {
