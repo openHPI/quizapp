1688d73fb0363ac9229abec875865bbeacffb3cb
Franz Liedke <franz.liedke@student.hpi.de>
2017-03-10 14:50:33 +0100

    Use latest nightly of fixture adapter library

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,15 +4,5 @@ import FixtureAdapter from 'ember-data-fixture-adapter';
 export default FixtureAdapter.extend({
   plurals: {
     'quiz':'quizzes'
-  },
-  queryFixtures(fixtures, query) {
-    const key = Ember.keys(query)[0];
-    return fixtures.filterBy(key, query[key]);
-  },
-  findRecord(store, typeClass, id) {
-    return this.find(store, typeClass, id);
-  },
-  query(store, typeClass, query) {
-    return this.findQuery(store, typeClass, query);
   }
 });
