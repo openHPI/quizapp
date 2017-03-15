9cd707fc7f6c0a9e20f06ca9e2902aee176b6166
Franz Liedke <franz.liedke@student.hpi.de>
2017-03-15 14:57:01 +0100

    progress-bar: Cancel animation properly on destroy

--- app/components/progress-bar.js
+++ app/components/progress-bar.js
@@ -5,7 +5,12 @@ export default Ember.Component.extend({
     this._super(...arguments);
 
     this.$('.bar').css('width', '100%');
-    this.$('.bar').stop();
     this.$('.bar').animate({width: '0px'}, this.time*1000, 'linear', this.attrs.whenTimeUp);
+  },
+
+  willDestroyElement() {
+    this._super(...arguments);
+
+    this.$('.bar').stop();
   }
 });
