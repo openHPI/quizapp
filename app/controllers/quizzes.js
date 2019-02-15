9458943646e52cae7a789edeb3230475b56af6ef
Christiane Hagedorn <hagedorncm@gmail.com>
2019-02-15 02:08:32 +0100

    Quiz-Swiper hinzugefügt

--- app/controllers/quizzes.js
+++ app/controllers/quizzes.js
@@ -3,11 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   game: Ember.inject.service(),
 
+  currentSlide: 0,
   quizzes: Ember.computed.alias('model'),
   players: Ember.computed.alias('game.players'),
 
+  fullPath: Ember.computed('currentSlide', function() {
+    return '/images/course' + this.get('currentSlide') + '.jpeg';
+  }),
+
   actions: {
-    selectQuiz(quiz, player) {
+    selectCurrentQuiz(player) {
+      let quiz=this.get('quizzes').objectAt(this.get('currentSlide'));
       console.log('player', player.get('name'), 'selected the quiz', quiz.get('title'));
       player.set('joined', true);
       player.set('active', false);
